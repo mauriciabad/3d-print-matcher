c2c7cd5abfc3cfc7a1ecb13a25b56f0561e94d32
Maurici Abad <maurici.abad@glovoapp.com>
2021-11-27 18:15:12 +0100

    Update rules

--- matcher-client/.eslintrc.js
+++ matcher-client/.eslintrc.js
@@ -4,17 +4,24 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': ['error', require('./prettier.config.js')],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-tags-order': [
+      'error',
+      { order: ['script', 'template', 'style'] },
+    ],
+    'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+    'prefer-template': 'warn',
   },
-};
+}
