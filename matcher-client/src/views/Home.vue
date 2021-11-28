44305fde764f91b1824bc3d75ebf17d598cde86c
Maurici Abad <maurici.abad@glovoapp.com>
2021-11-28 12:08:40 +0100

    use file

--- matcher-client/src/views/Home.vue
+++ matcher-client/src/views/Home.vue
@@ -21,7 +21,7 @@ export default defineComponent({
       }
 
       const formData = new FormData()
-      formData.append('image', image)
+      formData.append('file', image)
 
       try {
         print.value = await api.findMatch(formData)
